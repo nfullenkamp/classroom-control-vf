b4de041767a770d912a371be3d7463bd0c3b3be1
nfullenkamp <nfullenkamp@misoenergy.org>
2016-02-22 17:47:03 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Yo! My name is ${::hostname}": }
+  
+file { '/etc/motd':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  content => "Today I learned what it means to manage state using Puppet.\n",
+}
+  
 }
