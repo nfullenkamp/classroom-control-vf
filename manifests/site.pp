b48e371325a96ff0e249a8ce1d533fc3cbe527f3
nfullenkamp <nfullenkamp@misoenergy.org>
2016-02-25 12:40:22 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,11 +43,15 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   # class { 'my_class': }
-  if $::virtual != 'physical' {
-    $vmname = capitalize($::virtual)
-    notify { "This is a ${vmname} virtual machine.": }
-    # notify { "Yo! My name is ${::hostname}": }
-  }
+  # if $::virtual != 'physical' {
+  #  $vmname = capitalize($::virtual)
+  #  notify { "This is a ${vmname} virtual machine.": }
+  #  # notify { "Yo! My name is ${::hostname}": }
+  #  
+  #
+  # }
+  $message = hiera('message')
+  notify { $message: }
 }
   
 # include users
